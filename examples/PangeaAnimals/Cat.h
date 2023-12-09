0fe7bd408813ad12d4150f8fe81fff5a6fb1a1bb
Majid Darvishan <majid.darvishan@peykasa.ir>
2023-12-09 16:08:28 +0330

    init files

---
+++ examples/PangeaAnimals/Cat.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "../Zoo/Animal.h"
+
+class Cat : public Animal
+{
+public:
+    std::string kind() {return "Cat";}
+    bool can_swim() {return false;}
+};
+
+class CatDriver : public AnimalDriver
+{
+public:
+    CatDriver() : AnimalDriver("CatDriver", Cat::version) {}
+    Animal* create() {return new Cat();}
+};
